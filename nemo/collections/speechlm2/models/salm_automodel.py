d24e96846e01745e896684a84ecec38d462cb750
pzelasko <pzelasko@users.noreply.github.com>
2026-03-19 16:24:45 +0000

    Apply isort and black reformatting
    Signed-off-by: pzelasko <pzelasko@users.noreply.github.com>

--- nemo/collections/speechlm2/models/salm_automodel.py
+++ nemo/collections/speechlm2/models/salm_automodel.py
@@ -50,7 +50,9 @@ class SALMAutomodel(LightningModule, HFHubMixin):
         self.cfg = DictConfig(cfg)
         self.audio_locator_tag = self.cfg.audio_locator_tag
 
-        self.tokenizer = AutoTokenizer(self.cfg.pretrained_llm, use_fast=True, trust_remote_code=self.cfg.get("trust_remote_code", False))
+        self.tokenizer = AutoTokenizer(
+            self.cfg.pretrained_llm, use_fast=True, trust_remote_code=self.cfg.get("trust_remote_code", False)
+        )
         self.tokenizer.add_special_tokens({"additional_special_tokens": [self.audio_locator_tag]})
         self.llm = None  # populated by configure_model
         self.perception = None  # populated by configure_model
